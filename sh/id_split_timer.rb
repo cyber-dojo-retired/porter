1d94db9950522b236984fbb706697e6ca99a1c3f
Jon Jagger <jon@jaggersoft.com>
2018-10-07 15:46:26 +0100

    save-point

--- sh/id_split_timer.rb
+++ sh/id_split_timer.rb
@@ -20,35 +20,51 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 
-def setup_dirs(split)
-  # eg split = [3,2,1]
-  tmp = ARGV[0] + '/id_splits'
-  `rm -rf #{tmp}`
-
-  all_dirs = [ tmp ]
-  r10_dirs = [ tmp ]
-  split.each do |digits|
-    sss = (0...10**digits)     .map{ |n| zerod(n,digits) } # eg [00..99]
-    r10 = (0...10).to_a.shuffle.map{ |n| zerod(n,digits) } # eg [00..09]
-    all_dirs = splice(all_dirs, sss)
-    r10_dirs = splice(r10_dirs, r10)
-  end
-
-  # TODO: create all_dirs
-
-  # TODO: write known file into each r10 dirs
-
-  #puts "#{split}==> #{all_dirs.size}"
-
-  # return only those dirs that exist and have file in them
-  r10_dirs
+def zerod(n, digits)
+  "%0#{digits}d" % n
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 
-def zerod(n, digits)
-  "%0#{digits}d" % n
+def make_new_dir_names(digits)
+  (0...10**digits).map{ |n| zerod(n,digits) }
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+
+$new_dir_names = []
+
+def new_dir_names(n)
+  # eg n==1 --> [0..9]
+  # eg n==2 --> [00..99]
+  # eg n==3 --> [000..999]
+  $new_dir_names[n] ||= make_new_dir_names(n)
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+
+def setup_dirs(split)
+  # eg split = [3,2,1]
+  tmp = ARGV[0] + "/id_splits"
+  `rm -rf #{tmp}`
+
+  sample_dirs = [ tmp ]
+
+  split.each do |digits|
+    all_dirs = splice(sample_dirs, new_dir_names(digits))
+    # TODO: make these dirs
+
+    samples = (0...3).to_a.shuffle.map{ |n| zerod(n,digits) } # eg [00..05]
+    sample_dirs = all_dirs.select{|dir|
+      samples.any?{|sample| dir.end_with?(sample) }
+    }
+  end
+
+  # TODO: write known file into each r10 dirs
+  sample_dirs
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
 
 def splice(lhs,rhs)
   lhs.map do |a|
@@ -58,6 +74,8 @@ def splice(lhs,rhs)
     end
   end.flatten(1)
 end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
 
 $tally = 0
 
@@ -86,18 +104,8 @@ end
 # - - - - - - - - - - - - - - - - - - - - - - -
 
 def average_of(times)
-  times.reduce(:+) / times.size.to_f
-end
-
-# - - - - - - - - - - - - - - - - - - - - - - -
-
-def split_times(splits)
-  Hash[splits.map { |split|
-    times = setup_dirs(split).map { |dir|
-      timed { yield dir }
-    }
-    [split, '%.06f' % average_of(times)]
-  }]
+  mean = times.reduce(:+) / times.size.to_f
+  '%.06f' % mean
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
@@ -114,12 +122,33 @@ end
 # - - - - - - - - - - - - - - - - - - - - - - -
 
 n = ARGV[1].to_i
+puts("split times for #{n}")
+
 splits = partitions(n).collect{ |p| p.permutation
                                      .sort
                                      .uniq }
                       .flatten(1)
 
-STDOUT.puts("split times for #{n}")
-show_times('exists?', split_times(splits) {|dir| exists?(dir) })
-show_times('read',    split_times(splits) {|dir| read(dir) })
-show_times('write',   split_times(splits) {|dir| write(dir) })
+$exists_times = {}
+$read_times = {}
+$write_times = {}
+
+splits.each do |split|
+  sample_dirs = setup_dirs(split)
+
+  times = sample_dirs.map{ |dir| timed { exists?(dir) }}
+  time = average_of(times)
+  $exists_times[split] = time
+
+  times = sample_dirs.map{ |dir| timed { read(dir) }}
+  time = average_of(times)
+  $read_times[split] = time
+
+  times = sample_dirs.map{ |dir| timed { write(dir) }}
+  time = average_of(times)
+  $write_times[split] = time
+end
+
+show_times('exists?', $exists_times)
+show_times('read',    $read_times)
+show_times('write',   $write_times)
