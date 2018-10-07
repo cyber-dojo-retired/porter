81775f2f000328bcbce7f6e1b1d6c597f7b8b018
Jon Jagger <jon@jaggersoft.com>
2018-10-07 11:29:24 +0100

    work-in-progress save-point

--- sh/id_split_timer.rb
+++ sh/id_split_timer.rb
@@ -13,13 +13,32 @@ end
 
 def timed
   started = Time.now
-  result = yield
+  yield
   finished = Time.now
-  duration = '%.4f' % (finished - started)
-  [result,duration]
+  finished - started
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
+
+def setup_dirs(one)
+  # eg one = [3,2,1]
+  r10 = (0..10).to_a.shuffle
+  # do dirs setup... using r10 for each level
+  # write known file into each dir
+
+  # digits = 2
+  # (0..10).to_a.shuffle.map{ |n| "%0#{digits}d" % n }
+
+  [ '/tmp/000/07/9', '/tmp/009/01/4' ]
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+
+def average_of(all)
+  all.reduce(:+) / all.size.to_f
+end
+
+# = = = = = = = = = = = = = = = = = = = = = = =
 
 def timed_reads(all)
   Hash[all.map {|one| [one,timed_read(one)] }]
@@ -29,11 +48,12 @@ end
 
 def timed_read(one)
   # eg one = [3,2,1]
-  # do setup...
-  result,duration = timed {
-    sleep 0.01
-  }
-  duration
+  times = setup_dirs(one).map { |dir_name| timed { do_read(dir_name) }}
+  average_of(times)
+end
+
+def do_read(dir_name)
+  sleep 0.01
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
