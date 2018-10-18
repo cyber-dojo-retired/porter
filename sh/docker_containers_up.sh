f2434259af89f98d4509101d4a9e275f450e1188
Jon Jagger <jon@jaggersoft.com>
2018-10-18 20:43:51 +0100

    set ownership of host dirs that are volume-mounted into saver and porter

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -36,29 +36,28 @@ wait_till_up "test-${MY_NAME}-storer"
 wait_till_up "test-${MY_NAME}-saver"
 
 # - - - - - - - - - - - - - - - - - - - -
+# storer holds state as a volume which has already set ownership
 
 docker exec \
   --user root \
     "test-${MY_NAME}-storer" \
       sh -c 'cd /usr/src/cyber-dojo/katas && rm -rf *'
 
-docker exec \
-  --user root \
-    "test-${MY_NAME}-saver" \
-      sh -c 'cd /groups && rm -rf *'
+# saver holds state as a host dir, so needs to set ownership
 
 docker exec \
   --user root \
     "test-${MY_NAME}-saver" \
-      sh -c 'cd /groups && rm -rf *'
+      sh -c 'cd /groups && rm -rf * && chown -R 19663:19663 /groups'
 
 docker exec \
   --user root \
     "test-${MY_NAME}-saver" \
-      sh -c 'cd /katas && rm -rf *'
+      sh -c 'cd /katas && rm -rf * && chown -R 19663:19663 /katas'
+
+# porter holds state as a host dir, so needs to set ownership
 
 docker exec \
   --user root \
     "test-${MY_NAME}-server" \
-      sh -c 'cd /id-map && rm -rf *'
-
+      sh -c 'cd /id-map && rm -rf * && chown -R 19664:19664 /id-map'
