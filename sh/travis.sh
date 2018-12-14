12a439ea540315e3a32bdeb2aa28bc629d38d93a
Jon Jagger <jon@jaggersoft.com>
2018-12-14 19:51:16 +0000

    simplify docker-compose so it uses tmpfs instead of volume-mounts

--- sh/travis.sh
+++ sh/travis.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ev
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  pwd
   ./pipe_build_up_test.sh
   curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
   chmod +x push_and_trigger.sh
