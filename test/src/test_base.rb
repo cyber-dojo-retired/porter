75aa9cc5416aa508e7e1a4e22370ef8195581c40
Jon Jagger <jon@jaggersoft.com>
2018-12-15 11:47:40 +0000

    refactoring; collapse test duplication

--- test/src/test_base.rb
+++ test/src/test_base.rb
@@ -16,29 +16,28 @@ class TestBase < HexMiniTest
   # - - - - - - - - - - - - - - - - -
 
   def assert_ports_with_matching_id(id10)
-    id6 = id10[0..5]
-    assert storer.kata_exists?(id10), id10
-    refute saver.group_exists?(id6), id10
-    was = was_data(id10)
-    gid = port(id10)
-    assert_equal id6, gid, id10
-    assert saver.group_exists?(gid), id10
-    now = now_data(gid)
-    refute storer.kata_exists?(id10), id10
-    assert_ported(was, now, id10)
-    print '.'
-    STDOUT.flush
+    assert_ports(id10) do |id6,gid|
+      assert_equal id6,gid,id10
+    end
   end
 
   # - - - - - - - - - - - - - - - - -
 
   def assert_ports_with_different_id(id10)
+    assert_ports(id10) do |id6,gid|
+      refute_equal id6,gid,id10
+    end
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  def assert_ports(id10)
     id6 = id10[0..5]
     assert storer.kata_exists?(id10), id10
     refute saver.group_exists?(id6), id10
     was = was_data(id10)
     gid = port(id10)
-    refute_equal id6, gid, id10
+    yield id6,gid #refute_equal id6, gid, id10
     assert saver.group_exists?(gid), id10
     now = now_data(gid)
     refute storer.kata_exists?(id10), id10
