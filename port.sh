3a1ea180e9fe656d49a2dbfe56800b0ecf7a8230
Jon Jagger <jon@jaggersoft.com>
2018-12-17 19:58:57 +0000

    add port.sh and port.rb files

---
+++ port.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# checker docker installed
+# check data-container exists
+# check storer service is UP
+# bring up saver service
+# check saver uid has write access to /cyber-dojo
+# bring up porter container
+# docker exec -it porter-container sh -c 'ruby port.rb ${*}'
+
+# always remove porter container
+# always remove saver container
---
+++ src/port.rb
@@ -0,0 +1,4 @@
+
+# port one id-10 => port_one(id-10)
+# port many id-2 => port_many(id-2)
+# port all       => port_all
