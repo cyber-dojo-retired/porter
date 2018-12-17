d9504ed4421f3d562cea8bbe2dc30e296406c9c6
Jon Jagger <jon@jaggersoft.com>
2018-12-17 20:14:02 +0000

    pseudo-code notes

--- port.sh
+++ port.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # checker docker installed
+
 # check data-container exists
-# check storer service is UP
-# bring up saver service
-# check saver uid has write access to /cyber-dojo
-# bring up porter container
-# docker exec -it porter-container sh -c 'ruby port.rb ${*}'
+# check storer service is NOT already up
+# docker pull cyberdojo/storer (to get eg kata_delete)
+# bring up storer service
+
+# docker pull cyberdojo/saver
+# make sure /cyber-dojo dir exists
+# bring up saver service and volume-mount /cyber-dojo dir
+# check saver-uid has write access to /cyber-dojo (with docker exec)
+#    (if on DockerToolbox with will be on default VM)
+
+# docker pull cyberdojo/porter
+# make sure /id-map exists (??? OR PUT JSON FILES IN /tmp ???)
+# bring up porter container - needs to link to storer and saver
+# check porter-uid has write access to /id-map (with docker exec)
+#    (if on DockerToolbox with will be on default VM)
+# docker exec -it porter-container sh -c 'ruby /app/port.rb ${*}'
 
 # always remove porter container
 # always remove saver container
--- src/port.rb
+++ src/port.rb
@@ -1,4 +1,17 @@
 
-# port one id-10 => port_one(id-10)
-# port many id-2 => port_many(id-2)
-# port all       => port_all
+# port id-10 => port_one(id-10)
+
+# port id-2 => port_many(id-2)
+#    use kata_completions(id-2)
+
+# port      => port_all
+#    use all 58x58 generated id-2's
+
+# - - - - - - - - - - - - - - - - - - - - -
+# port and individual id
+# if ok
+#    print .
+# if an exeption
+#    print E and add id info to json? file (named from date+time)
+# if id-mapped
+#    print M and add id info to json? file (named from date+time)
