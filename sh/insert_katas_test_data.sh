daf4b91bf200fb49bed01675dff8eca70ec79099
Jon Jagger <jon@jaggersoft.com>
2018-12-19 09:52:00 +0000

    untar storer-test data to dir ready to volume-mount into storer

--- sh/insert_katas_test_data.sh
+++ sh/insert_katas_test_data.sh
@@ -3,6 +3,17 @@ set -e
 
 # https://github.com/cyber-dojo/inserter
 
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+
+# untar to test_data_storer_context
+cd ${ROOT_DIR}/test_data_storer_context
+tar -xf ${ROOT_DIR}/test_data_storer/dup/463748A0E8.tgz
+tar -xf ${ROOT_DIR}/test_data_storer/dup/463748D943.tgz
+
+# volume-mount in docker-compose.yml
+
+sdfsdf
+
 docker run \
    --rm -it \
    --volume /var/run/docker.sock:/var/run/docker.sock \
@@ -10,3 +21,5 @@ docker run \
      test-porter-storer \
        dup old new red
        # 7E 4D
+
+# do few with volume-mount...
