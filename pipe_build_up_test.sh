519f4ec57b1eb9b96ddcb24aeca4a047243dd166
Jon Jagger <jon@jaggersoft.com>
2018-12-20 19:11:45 +0000

    added notes on different between server-tests and client-shell-tests

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -6,8 +6,15 @@ readonly MY_NAME="${ROOT_DIR##*/}"
 readonly SH_DIR="${ROOT_DIR}/sh"
 
 "${SH_DIR}/build_docker_images.sh"
-"${SH_DIR}/docker_containers_up.sh"
+# These tests run before containers have been brought up
+# and currently fail because /cyber-dojo and /porter
+# (wherever the docker-daemon is) are not yet setup.
 "${SH_DIR}/../client_shell/run_tests.sh"
+
+# These containers are started via docker-compose whose
+# entries give fake volume-mounts for /cyber-dojo and /porter
+# in tmpfs's with controlled onwership.
+"${SH_DIR}/docker_containers_up.sh"
 "${SH_DIR}/insert_katas_test_data.sh"
 if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
   "${SH_DIR}/docker_containers_down.sh"
