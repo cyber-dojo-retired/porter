58f0ae7fe360d9a7195f93ecc38f79850f3b19f0
Jon Jagger <jon@jaggersoft.com>
2018-12-20 13:18:00 +0000

    improve diagnostic messages in up.sh

--- up.sh
+++ up.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-readonly name=porter
+readonly name=porter # of the service
 readonly dir=porter
 readonly uid=19664
+readonly user_name=porter
+readonly gid=65534
+readonly group_name=nogroup
 
 if [[ ! -d /${dir} ]]; then
+  cmd="mkdir /${dir}"
   echo "ERROR"
   echo "The ${name} service needs to volume-mount /${dir} on the host"
-  echo "Please run"
-  echo "  \$ [sudo] mkdir /${dir}"
-  echo "If you are running on Docker-Toolbox"
+  echo "Please run:"
+  echo "  \$ [sudo] ${cmd}"
   if [ ! -z ${DOCKER_MACHINE_NAME} ]; then
-    echo "(and it looks like you are)"
+    echo "If you are running on Docker-Toolbox (and it looks like you are)"
+    echo "remember to run this on the target VM. For example:"
+    echo "  \$ docker-machine ssh ${DOCKER_MACHINE_NAME} sudo ${cmd}"
   fi
-  echo "remember to run this on the target VM."
-  echo "For example"
-  echo "  \$ docker-machine ssh default sudo mkdir /${dir}"
   exit 1
 fi
 
 readonly probe="for-ownership"
 mkdir /${dir}/${probe} 2>/dev/null
 if [ $? -ne 0 ] ; then
+  cmd="chown ${uid}:${gid} /${dir}"
   echo "ERROR"
-  echo "The ${name} service (uid=${uid}) needs write access to /${dir}"
+  echo "The ${name} service needs write access to /${dir}"
+  echo "uid=${uid} (username=${user_name})"
+  echo "gid=${gid} (group=${group_name})"
   echo "Please run:"
-  echo "  $ [sudo] chown ${uid} /${dir}"
-  echo "If you are running on Docker-Toolbox"
+  echo "  $ [sudo] chown ${cmd}"
   if [ ! -z ${DOCKER_MACHINE_NAME} ]; then
-    echo "(and it looks like you are)"
+    echo "If you are running on Docker-Toolbox (and it looks like you are)"
+    echo "remember to run this on the target VM. For example:"
+    echo "  \$ docker-machine ssh ${DOCKER_MACHINE_NAME} sudo ${cmd}"
   fi
-  echo "remember to run this on the target VM."
-  echo "For example"
-  echo "  \$ docker-machine ssh default sudo chown ${uid} /${dir}"
   exit 2
 else
   rmdir /${dir}/${probe}
