1ac77a724b1ea13834dde44df05bf457f488cc6f
Jon Jagger <jon@jaggersoft.com>
2018-12-21 13:40:27 +0000

    allow specific client-shell tests to be run

--- client_shell/run_tests.sh
+++ client_shell/run_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+readonly test_files="${1:-test_*.sh}"
 
 failed=0
-for file in ${MY_DIR}/test_*.sh; do
+for file in ${my_dir}/${test_files}; do
   ${file}
   if [ $? != 0 ]; then
     failed=1
