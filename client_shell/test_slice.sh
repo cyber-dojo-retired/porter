184c5d9c35b2fc19caa4b8a98d5a4206331dcc14
Jon Jagger <jon@jaggersoft.com>
2018-12-21 15:10:01 +0000

    refactoring; collapse duplication

--- client_shell/test_slice.sh
+++ client_shell/test_slice.sh
@@ -7,12 +7,24 @@ readonly sh_dir="${my_dir}/sh"
 
 readonly dm_ssh="docker-machine ssh ${DOCKER_MACHINE_NAME}"
 
+get_image_name()
+{
+  local name=${1}
+  echo "cyber-dojo-porter-client-shell-${name}"
+}
+
+get_data_container_name()
+{
+  local name=${1}
+  local image_name=$(get_image_name "${name}")
+  echo "${image_name}-dc"
+}
+
 create_stub_storer_data_container()
 {
   local name=${1}
-  local image_name="cyber-dojo-porter-client-shell-${name}"
-  local dc_name="${image_name}-dc"
-
+  local image_name=$(get_image_name "${name}")
+  local dc_name=$(get_data_container_name "${name}")
   ${sh_dir}/docker_build_volumed_image.sh ${image_name}
   docker rm --force --volumes ${dc_name} > /dev/null 2>&1 || true
   ${sh_dir}/docker_create_data_container.sh ${image_name} ${dc_name}
@@ -22,9 +34,8 @@ create_stub_storer_data_container()
 create_root_dir_for_saver_volume_mount()
 {
   local name=${1}
-  local image_name="cyber-dojo-porter-client-shell-${name}"
-  local dc_name="${image_name}-dc"
-
+  local image_name=$(get_image_name "${name}")
+  local dc_name=$(get_data_container_name "${name}")
   export SAVER_HOST_ROOT_DIR=$(${dm_ssh} sudo mktemp -d /tmp/${dc_name}-saver.XXXXXX)
   $(${dm_ssh} sudo mkdir ${SAVER_HOST_ROOT_DIR}/cyber-dojo)
   $(${dm_ssh} sudo chown -R 19663:65533 ${SAVER_HOST_ROOT_DIR})
@@ -33,9 +44,8 @@ create_root_dir_for_saver_volume_mount()
 create_root_dir_for_porter_volume_mount()
 {
   local name=${1}
-  local image_name="cyber-dojo-porter-client-shell-${name}"
-  local dc_name="${image_name}-dc"
-
+  local image_name=$(get_image_name "${name}")
+  local dc_name=$(get_data_container_name "${name}")
   export PORTER_HOST_ROOT_DIR=$(${dm_ssh} sudo mktemp -d /tmp/${dc_name}-porter.XXXXXX)
   $(${dm_ssh} sudo mkdir ${PORTER_HOST_ROOT_DIR}/porter)
   $(${dm_ssh} sudo chown -R 19664:65533 ${PORTER_HOST_ROOT_DIR})
@@ -44,8 +54,8 @@ create_root_dir_for_porter_volume_mount()
 cleanup()
 {
   local name=${1}
-  local image_name="cyber-dojo-porter-client-shell-${name}"
-  local dc_name="${image_name}-dc"
+  local image_name=$(get_image_name "${name}")
+  local dc_name=$(get_data_container_name "${name}")
   # TODO: call in trap
   $(${dm_ssh} sudo rm -rf ${SAVER_HOST_ROOT_DIR})
   $(${dm_ssh} sudo rm -rf ${PORTER_HOST_ROOT_DIR})
