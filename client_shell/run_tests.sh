d1f90ae86b1f79eb0a1a47166b5df01e279735b4
Jon Jagger <jonjagger@Jons-MBP.lan>
2018-12-22 06:40:55 +0000

    make it easy to run individual client-shell tests

--- client_shell/run_tests.sh
+++ client_shell/run_tests.sh
@@ -2,8 +2,8 @@
 set -e
 
 readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-readonly test_files="${1:-test_*.sh}"
+readonly glob="${1}"
 
-for test_file in ${my_dir}/${test_files}; do
+for test_file in ${my_dir}/test_${glob}*; do
   ${test_file}
 done
