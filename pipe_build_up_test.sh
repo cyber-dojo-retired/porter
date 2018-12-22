1cd8e3722411d42abb3f37df369462cc128ffd28
Jon Jagger <jon@jaggersoft.com>
2018-12-22 15:48:56 +0000

    add circlec files

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly MY_NAME="${ROOT_DIR##*/}"
-readonly SH_DIR="${ROOT_DIR}/sh"
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && cd sh && pwd )"
 
 "${SH_DIR}/build_docker_images.sh"
-"${SH_DIR}/../client_shell/run_tests.sh" "$@"
-exit 0
-
 "${SH_DIR}/docker_containers_up.sh"
 "${SH_DIR}/insert_katas_test_data.sh"
-if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
-  "${SH_DIR}/docker_containers_down.sh"
-  docker rmi "cyberdojo/${MY_NAME}-client" > /dev/null 2>&1
-  docker image prune --force > /dev/null 2>&1
-fi
+"${SH_DIR}/run_tests_in_containers.sh" "$@"
+"${SH_DIR}/docker_containers_down.sh"
+
+#"${SH_DIR}/../client_shell/run_tests.sh" "$@"
