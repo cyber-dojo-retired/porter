24a745554e8976fe97e755462ef76937c2b57996
Jon Jagger <jonjagger@Jons-MBP.lan>
2018-12-22 06:49:04 +0000

    prepare for sh fn that takes into account docker-machine env-var

--- client_shell/porter_helpers.sh
+++ client_shell/porter_helpers.sh
@@ -2,7 +2,6 @@
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 readonly PORT_STORER_2_SAVER=${MY_DIR}/../port_cyber_dojo_storer_to_saver.sh
-readonly dm_ssh="docker-machine ssh ${DOCKER_MACHINE_NAME}"
 
 port()
 {
@@ -10,6 +9,8 @@ port()
   status=$?
   echo ${status} >${statusF}
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
 
 get_image_name()
 {
@@ -23,6 +24,8 @@ get_data_container_name()
   local image_name=$(get_image_name "${name}")
   echo "${image_name}-dc"
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
 
 create_stub_storer_data_container()
 {
@@ -47,14 +50,24 @@ create_stub_storer_data_container()
   export STORER_DATA_CONTAINER_NAME=${dc_name}
 }
 
+# - - - - - - - - - - - - - - - - - - - - - - - -
+
+readonly dm_ssh="docker-machine ssh ${DOCKER_MACHINE_NAME}"
+
+on_host()
+{
+  local cmd="${1}"
+  $(${dm_ssh} sudo ${cmd})
+}
+
 create_root_dir_for_saver_volume_mount()
 {
   local name=${1}
   local image_name=$(get_image_name "${name}")
   local dc_name=$(get_data_container_name "${name}")
   export SAVER_HOST_ROOT_DIR=$(${dm_ssh} sudo mktemp -d /tmp/${dc_name}-saver.XXXXXX)
-  $(${dm_ssh} sudo mkdir ${SAVER_HOST_ROOT_DIR}/cyber-dojo)
-  $(${dm_ssh} sudo chown -R 19663:65533 ${SAVER_HOST_ROOT_DIR})
+  on_host "mkdir ${SAVER_HOST_ROOT_DIR}/cyber-dojo"
+  on_host "chown -R 19663:65533 ${SAVER_HOST_ROOT_DIR}"
 }
 
 create_root_dir_for_porter_volume_mount()
@@ -63,9 +76,11 @@ create_root_dir_for_porter_volume_mount()
   local image_name=$(get_image_name "${name}")
   local dc_name=$(get_data_container_name "${name}")
   export PORTER_HOST_ROOT_DIR=$(${dm_ssh} sudo mktemp -d /tmp/${dc_name}-porter.XXXXXX)
-  $(${dm_ssh} sudo mkdir ${PORTER_HOST_ROOT_DIR}/porter)
-  $(${dm_ssh} sudo chown -R 19664:65533 ${PORTER_HOST_ROOT_DIR})
+  on_host "mkdir ${PORTER_HOST_ROOT_DIR}/porter"
+  on_host "chown -R 19664:65533 ${PORTER_HOST_ROOT_DIR}"
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
 
 cleanup()
 {
@@ -73,8 +88,8 @@ cleanup()
   local image_name=$(get_image_name "${name}")
   local dc_name=$(get_data_container_name "${name}")
   # TODO: call in trap
-  $(${dm_ssh} sudo rm -rf ${SAVER_HOST_ROOT_DIR})  > /dev/null 2>&1 || true
-  $(${dm_ssh} sudo rm -rf ${PORTER_HOST_ROOT_DIR}) > /dev/null 2>&1 || true
+  on_host "rm -rf ${SAVER_HOST_ROOT_DIR}  > /dev/null 2>&1 || true"
+  on_host "rm -rf ${PORTER_HOST_ROOT_DIR} > /dev/null 2>&1 || true"
   docker container rm --force --volumes ${dc_name} > /dev/null 2>&1 || true
   docker image rm --force ${image_name}            > /dev/null 2>&1 || true
 }
