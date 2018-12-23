94f74f6c6bb7fc14de2e9dde1b5c4e44fba01220
Jon Jagger <jon@jaggersoft.com>
2018-12-23 10:11:56 +0000

    add client-shell test for multiple unknown args

---
+++ client_shell/test_008_unknown_args_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+
+. ${my_dir}/porter_helpers.sh
+
+test_008_unknown_args()
+{
+  local name=008
+  create_stub_storer_data_container ${name}
+  create_stub_saver_volume_mount_root_dir ${name}
+  create_stub_porter_volume_mount_root_dir ${name}
+
+  port alpha beta
+  cleanup_stubs ${name}
+
+  #assert_stdout_equals ''
+  assert_stderr_includes 'ERROR: unknown arg <alpha>'
+  assert_stderr_includes 'ERROR: unknown arg <beta>'
+  assert_status_equals 10
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+. ${my_dir}/shunit2_helpers.sh
+. ${my_dir}/shunit2
