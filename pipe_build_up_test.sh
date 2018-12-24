f76e38969e2d07d803f6cdd2aa866e97bd2bf6ef
Jon Jagger <jon@jaggersoft.com>
2018-12-24 13:43:30 +0000

    docker pull storer/saver images before running shell-tests so you dont get extra lines on stdout/stderr which interfere with test assertions

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -4,6 +4,8 @@ set -e
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && cd sh && pwd )"
 
 "${SH_DIR}/build_docker_images.sh"
+docker pull cyberdojo/storer
+docker pull cyberdojo/saver
 "${SH_DIR}/../client_shell/run_tests.sh" "$@"
 exit 0
 "${SH_DIR}/docker_containers_up.sh"
