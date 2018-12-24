0dd23e8cf4ada020e9614671ed4c7100e77dc5fc
Jon Jagger <jon@jaggersoft.com>
2018-12-24 16:19:57 +0000

    check how docker is running on Travis

--- sh/travis.sh
+++ sh/travis.sh
@@ -2,6 +2,7 @@
 set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  echo "DOCKER_MACHINE_NAME=:${DOCKER_MACHINE_NAME}:"
   ./pipe_build_up_test.sh
   curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
   chmod +x push_and_trigger.sh
