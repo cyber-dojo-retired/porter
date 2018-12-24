e1b4b81075853daa85dce112f7b74aa81e899919
Jon Jagger <jon@jaggersoft.com>
2018-12-24 11:45:10 +0000

    add test for --id10 as user sees it

--- client_shell/test_011_sample_id10_some_katas.sh
+++ client_shell/test_011_sample_id10_some_katas.sh
@@ -4,9 +4,9 @@ readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 . ${my_dir}/porter_helpers.sh
 
-test_011_sample_id10_no_katas()
+test_011_sample_id10_some_katas()
 {
-  local name=011
+  local name=011a
   create_stub_storer_data_container ${name}
   create_stub_saver_volume_mount_root_dir ${name}
   create_stub_porter_volume_mount_root_dir ${name}
@@ -24,6 +24,31 @@ test_011_sample_id10_no_katas()
 
   assert_stdout_includes_all_up_down
   assert_stdout_line_count_equals 18 # 17 + 1 which is the sample!
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+
+test_011_sample_id10_some_katas_no_info()
+{
+  local name=011b
+  create_stub_storer_data_container ${name}
+  create_stub_saver_volume_mount_root_dir ${name}
+  create_stub_porter_volume_mount_root_dir ${name}
+
+  local dc_name=$(get_data_container_name "${name}")
+  local cid=$(docker run --detach --interactive --name jj \
+    --volumes-from ${dc_name} \
+      cyberdojo/storer sh)
+  ${my_dir}/../inserter/insert.sh jj old
+  docker container rm --force ${cid} > /dev/null
+
+  port --id10
+  cleanup_stubs ${name}
+
+  cat ${stdoutF}
+  assert_stdout_line_count_equals 1
   assert_stderr_equals ''
   assert_status_equals 0
 }
