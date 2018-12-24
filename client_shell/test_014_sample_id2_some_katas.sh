4e2c55711aec9e7e1d987da9da910d2f529f6103
Jon Jagger <jon@jaggersoft.com>
2018-12-24 13:29:39 +0000

    prepare next set of tests mostly for id2

---
+++ client_shell/test_014_sample_id2_some_katas.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+readonly my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+
+. ${my_dir}/porter_helpers.sh
+
+test_014_sample_id2_some_katas_with_porter_info()
+{
+  local name=014a
+  create_stub_storer_data_container ${name}
+  create_stub_saver_volume_mount_root_dir ${name}
+  create_stub_porter_volume_mount_root_dir ${name}
+  insert_kata_data_in_storer_data_container ${name} old
+
+  export SHOW_PORTER_INFO=true
+  port --id2
+  cleanup_stubs ${name}
+
+  assert_stdout_includes_all_up_down
+  assert_stdout_line_count_equals 18 # 17 + 1 which is the sample!
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+
+test_014_sample_id2_some_katas_as_user_sees_it()
+{
+  local name=014b
+  create_stub_storer_data_container ${name}
+  create_stub_saver_volume_mount_root_dir ${name}
+  create_stub_porter_volume_mount_root_dir ${name}
+  insert_kata_data_in_storer_data_container ${name} old
+
+  export SHOW_PORTER_INFO=false
+  port --id2
+  cleanup_stubs ${name}
+
+  local id2="`cat ${stdoutF}`"
+  assert_id2 "${id2}"
+  assert_stdout_line_count_equals 1
+  assert_stderr_equals ''
+  assert_status_equals 0
+}
+
+. ${my_dir}/shunit2_helpers.sh
+. ${my_dir}/shunit2
