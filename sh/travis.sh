b2d552157cfbf8f4d3bdbbf9ecfe5770686624fa
Jon Jagger <jon@jaggersoft.com>
2018-12-24 16:21:21 +0000

    porter is not used in web so dont trigger it

--- sh/travis.sh
+++ sh/travis.sh
@@ -4,7 +4,7 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo "DOCKER_MACHINE_NAME=:${DOCKER_MACHINE_NAME}:"
   ./pipe_build_up_test.sh
-  curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
-  chmod +x push_and_trigger.sh
-  ./push_and_trigger.sh cyber-dojo/web
+  #curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
+  #chmod +x push_and_trigger.sh
+  #./push_and_trigger.sh cyber-dojo/web
 fi
