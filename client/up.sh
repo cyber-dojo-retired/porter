5328223a48cdd7f808b386cc9518b802073b50d1
Jon Jagger <jon@jaggersoft.com>
2018-12-25 11:52:48 +0000

    refactoring; tidy client-test's Dockerfile, up.sh

--- client/up.sh
+++ client/up.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-set -e
 
-# Note that the --host is needed for IPv4 and IPv6 addresses
-
-bundle exec rackup \
-  --warn \
-  --host 0.0.0.0 \
-  --port 4518 \
-  --server thin \
+rackup             \
   --env production \
+  --host 0.0.0.0   \
+  --port 4518      \
+  --server thin    \
+  --warn           \
     config.ru
