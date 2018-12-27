5fa15dbcce17384e4382bc5ccf08910cdea85646
Jon Jagger <jon@jaggersoft.com>
2018-12-27 22:17:00 +0000

    do docker push in travis.sh file

--- sh/travis.sh
+++ sh/travis.sh
@@ -2,8 +2,9 @@
 set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "DOCKER_MACHINE_NAME=:${DOCKER_MACHINE_NAME}:"
   ./pipe_build_up_test.sh
+  echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
+  docker push cyberdojo/porter
   #curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
   #chmod +x push_and_trigger.sh
   #./push_and_trigger.sh cyber-dojo/web
