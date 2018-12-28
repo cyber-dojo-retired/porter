f343f3208b43b463db662761c8d691390896c7a6
Jon Jagger <jon@jaggersoft.com>
2018-12-28 15:51:06 +0000

    fix manifest['exercise'] erroneously set to null/nil for some custom sessions

--- src/porter.rb
+++ src/porter.rb
@@ -79,6 +79,10 @@ class Porter
     manifest['visible_files'].transform_values!{ |content|
       { 'content' => content }
     }
+    if manifest['exercise'].nil?
+      # some custom sessions erroneously set exercise (to null)
+      manifest.delete('exercise')
+    end
   end
 
   private
