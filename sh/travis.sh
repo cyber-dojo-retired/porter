69d519989f4a519702ffa5fcb58182daec171139
Jon Jagger <jon@jaggersoft.com>
2018-12-30 14:02:34 +0000

    dont push inserter image in travis script as its now built in its own repo

--- sh/travis.sh
+++ sh/travis.sh
@@ -7,8 +7,4 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   ./pipe_build_up_test.sh
   echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
   docker push cyberdojo/porter
-  docker push cyberdojo/inserter  
-  #curl -O https://raw.githubusercontent.com/cyber-dojo/cyber-dojo/master/shared/push_and_trigger.sh
-  #chmod +x push_and_trigger.sh
-  #./push_and_trigger.sh cyber-dojo/web
 fi
